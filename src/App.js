ea0797815643e8c132268b04aadf3af5d0c6a0ed
Abby F <abby_faigao@yahoo.com>
2022-06-18 08:19:53 -0700

    refactor to functional component

--- src/App.js
+++ src/App.js
@@ -1,40 +1,37 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import axios from "axios";
 import "./App.css";
 
-class App extends React.Component {
-  state = { advice: "" };
+function App2() {
+  const [advice, setAdvice] = useState("");
 
-  componentDidMount() {
-    this.fetchAdvice();
-  }
-
-  fetchAdvice = () => {
+  const fetchAdvice = () => {
     axios
-      .get("	https://api.adviceslip.com/advice")
+      .get("https://api.adviceslip.com/advice")
       .then((response) => {
         const { advice } = response.data.slip;
 
-        this.setState({ advice });
+        setAdvice(advice);
       })
       .catch((error) => {
         console.log(error);
       });
   };
 
-  render() {
-    const { advice } = this.state;
-    return (
-      <div className="app">
-        <div className="card">
-          <h1 className="heading">{advice}</h1>
-          <button className="button" onClick={this.fetchAdvice}>
-            <span>Give me advice!</span>
-          </button>
-        </div>
+  useEffect(() => {
+    fetchAdvice();
+  }, []);
+
+  return (
+    <div className="app">
+      <div className="card">
+        <h1 className="heading">{advice}</h1>
+        <button className="button" onClick={fetchAdvice}>
+          <span>Give me advice!</span>
+        </button>
       </div>
-    );
-  }
+    </div>
+  );
 }
 
-export default App;
+export default App2;
